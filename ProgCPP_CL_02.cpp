ace0d4e048732362fe21c3d771102f632886699a
camiiladarc <camiladarc94@hotmail.com>
2024-12-02 16:41:09 +0000

    Create ProgCPP_CL_02.cpp

---
+++ ProgCPP_CL_02.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+  int idade;
+  float altura;
+  count << "Insira sua idade:";
+  cin >> altura;
+  count << "Insira sua altura:"
+    cin >> altura;
+  cout << "Sua altura é " << idade << " e sua idade é: " << altura << endl;
+  return 0
+}
